ee363589bb0262de2629bc7f9174c4d6d9b8e3ae
Alexander Labrador <globehackers@gmail.com>
2015-08-04 11:12:27 -0430

    update

---
+++ inc/footer.php
@@ -0,0 +1,17 @@
+<footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy; 2016 <a target="_blank" href="http://groupnet.com.ve/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">ShapeBootstrap</a>. All Rights Reserved.
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Servicios</a></li>
+                        <li><a href="#">Faq</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+</footer><!--/#footer-->
